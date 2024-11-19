4f01e35b42f06f5933c8985c9cdc31f81e2d0aaa
pablitocavaz04 <pablocavaz2004@gmail.com>
2024-11-19 14:03:34 +0100

    Agregar archivos de proyecto.

---
+++ DI_UD3_9/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DI_UD3_9
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(Object sender, RoutedEventArgs e)
+        {
+            string s = null;
+            try
+            {
+                s.Trim();
+
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show("A handle exception just ocurred: " + ex.Message,
+                    "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(e.Uri.AbsoluteUri);
+            e.Handled = true;
+        }
+    }
+}
